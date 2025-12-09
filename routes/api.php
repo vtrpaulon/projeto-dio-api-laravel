cb23cf794b0d54a3255da99db875b7ed7a29c3d4
VitorPaulon <vtrpaulon@gmail.com>
2025-12-09 20:40:33 -0300

    ajustes

--- routes/api.php
+++ routes/api.php
@@ -3,4 +3,9 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PersonController;
 
-Route::apiResource('pessoas', PersonController::class);
+Route::get('/pessoas', [PersonController::class, 'index']);
+Route::post('/pessoas', [PersonController::class, 'store']);
+Route::get('/pessoas/{id}', [PersonController::class, 'show']);
+Route::put('/pessoas/{id}', [PersonController::class, 'update']);
+Route::delete('/pessoas/{id}', [PersonController::class, 'destroy']);
+
